1f375aa1a8197a0e1aabb49f0ab1412b48c6de6d
Jungas80 <juanhegert@gmail.com>
2022-11-17 00:15:55 -0300

    workflows7

--- database/tables/creates/tabla1.sql
+++ database/tables/creates/tabla1.sql
@@ -2,3 +2,8 @@ CREATE TABLE public.tabla_demo1 (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR NOT NULL
 );
+
+CREATE TABLE public.tabla_demo2 (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR NOT NULL
+);
