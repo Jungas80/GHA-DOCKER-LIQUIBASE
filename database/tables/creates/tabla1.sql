6d509ea437827c6642bdb5891fbebea0bab0da4c
Jungas80 <juanhegert@gmail.com>
2022-11-18 00:00:03 -0300

    18

--- database/tables/creates/tabla1.sql
+++ database/tables/creates/tabla1.sql
@@ -1,4 +1,7 @@
+--liquibase formatted sql
+--changeset juan:1
 CREATE TABLE public.tabla_demo1 (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR NOT NULL
 );
+--rollback drop table public.tabla_demo1;
